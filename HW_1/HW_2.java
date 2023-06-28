222fd6279abeb7640a8ce6ccb63c52c4e91a2524
SiberianCactus <kaktuz2101@mail.ru>
2023-06-29 00:04:45 +0700

    HW_2

---
+++ HW_1/HW_2.java
@@ -0,0 +1,12 @@
+// 2) Вывести все простые числа от 1 до 1000
+public class HW_2 {
+    public static void main(String[] args) {
+        ShowNumbers();
+}
+
+   public static void ShowNumbers() {
+        for (int i=1; i <= 1000 ; i++){
+            System.out.printf("%d ", i);
+        }
+    }
+}
